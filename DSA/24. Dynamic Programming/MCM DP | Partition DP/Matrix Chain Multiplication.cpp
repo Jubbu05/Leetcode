67b63537698d7581e3d49af326bb7a4682d00292
Jubbu05 <ayushnagpure23@gmail.com>
2023-06-28 17:11:03 +0530

    MCM DP | Partition DP

---
+++ DSA/24. Dynamic Programming/MCM DP | Partition DP/Matrix Chain Multiplication.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+int f(vector<int> &arr, int i, int j, vector<vector<int>> &dp)
+{
+
+    // base condition
+    if (i == j)
+        return 0;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    int mini = INT_MAX;
+
+    // partioning loop
+    for (int k = i; k <= j - 1; k++)
+    {
+
+        int ans = arr[i - 1] * arr[k] * arr[j] + f(arr, i, k, dp) + f(arr, k + 1, j, dp);
+
+        mini = min(mini, ans);
+    }
+
+    return mini;
+}
+
+int matrixMultiplication(vector<int> &arr, int N)
+{
+
+    vector<vector<int>> dp(N, vector<int>(N, -1));
+
+    int i = 1;
+    int j = N - 1;
+
+    return f(arr, i, j, dp);
+}
+
+// TABULATION
+#include <bits/stdc++.h>
+int matrixMultiplication(vector<int> &arr, int N)
+{
+
+    vector<vector<int>> dp(N, vector<int>(N, -1));
+
+    for (int i = 1; i < N; i++)
+    {
+        dp[i][i] = 0;
+    }
+
+    for (int i = N - 1; i >= 1; i--)
+    {
+
+        for (int j = i + 1; j < N; j++)
+        {
+
+            int mini = INT_MAX;
+
+            // partioning loop
+            for (int k = i; k <= j - 1; k++)
+            {
+
+                int ans = arr[i - 1] * arr[k] * arr[j] + dp[i][k] + dp[k + 1][j];
+
+                mini = min(mini, ans);
+            }
+
+            dp[i][j] = mini;
+        }
+    }
+
+    return dp[1][N - 1];
+}
---
+++ DSA/24. Dynamic Programming/MCM DP | Partition DP/Minimum Cost to Cut a Stick.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int rec(int i, int j, vector<int> &cuts, vector<vector<int>> &dp)
+    {
+        if (i > j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int mini = INT_MAX;
+
+        for (int k = i; k <= j; k++)
+        {
+                       // length of the stick        // left part of the stick     // right part of the
+            int cost = (cuts[j + 1] - cuts[i - 1]) + rec(i, k - 1, cuts, dp) + rec(k + 1, j, cuts, dp);
+            mini = min(mini, cost);
+        }
+
+        return dp[i][j] = mini;
+    }
+
+    int minCost(int n, vector<int> &cuts)
+    {
+        sort(cuts.begin(), cuts.end());
+
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(), 0);
+
+        vector<vector<int>> dp(cuts.size(), vector<int>(cuts.size(), -1));
+        return rec(1, cuts.size() - 2, cuts, dp);
+    }
+};
