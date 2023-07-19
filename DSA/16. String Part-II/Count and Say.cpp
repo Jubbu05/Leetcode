9d4351cf8fc58318a14193a2b320e620bec4bf25
Jubbu05 <ayushnagpure23@gmail.com>
2023-07-19 16:36:24 +0530

    update

--- DSA/16. String Part-II/Count and Say.cpp
+++ DSA/16. String Part-II/Count and Say.cpp
@@ -1,36 +1,37 @@
-class Solution {
+class Solution
+{
 public:
-    string countAndSay(int n) 
+  string countAndSay(int n)
+  {
+    if (n == 1)
+      return "1";
+    if (n == 2)
+      return "11";
+
+    string str = "11";
+
+    for (int i = 3; i <= n; i++)
     {
-        if(n == 1)
-          return "1";
-        if(n == 2)
-          return "11";
-          
-        string str = "11";
-        
-        for(int i = 3; i <= n; i++)
-         {
-             str += '$';
-             int len = str.length();
-             
-             int cnt = 1;
-             string tmp = "";
-             
-             for(int j = 1; j < len; j++)
-              {
-                  if(str[j] != str[j - 1])
-                    {
-                        tmp += to_string(cnt);
-                        tmp += str[j - 1];
-                        cnt = 1;
-                    }
-                   else
-                     cnt++;
-              }
-            str = tmp;  
-         }
-         
-        return str;
+      str += '$'; // delimiter
+      int len = str.length();
+
+      int cnt = 1;
+      string tmp = "";
+
+      for (int j = 1; j < len; j++)
+      {
+        if (str[j] != str[j - 1])
+        {
+          tmp += to_string(cnt); // count
+          tmp += str[j - 1]; // value
+          cnt = 1;
+        }
+        else
+          cnt++;
+      }`
+      str = tmp;
     }
+
+    return str;
+  }
 };
